1617c9136f7843921aded77492ec2e1c3aa9590c
Shawn McDearmon <shawn.a.mcdearmon@gmail.com>
2023-12-03 19:22:57 -0600

    working enemies and player

--- Main.gd
+++ Main.gd
@@ -3,27 +3,18 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
+func _ready():
+	new_game()
+
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-	$HUD.show_game_over()
 
 func new_game():
 	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-	$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
-
-func _on_score_timer_timeout():
-	score += 1
-	$HUD.update_score(score)
-
-func _on_start_timer_timeout():
-	$MobTimer.start()
-	$ScoreTimer.start()
-	$HUD.update_score(score)
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -49,3 +40,10 @@ func _on_mob_timer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+
+func _on_score_timer_timeout():
+	score += 1
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
--- Mob.gd
+++ Mob.gd
@@ -5,6 +5,5 @@ func _ready():
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 
-
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
